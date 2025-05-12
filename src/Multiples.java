0322d79b842d5c00bedd8319730d3b71614e4bc1
Amna Sami Ibrahim <amnasamiibrahim@gmail.com>
2025-05-12 13:41:50 -0400

    Finished task 4: Multiples.java

--- src/Multiples.java
+++ src/Multiples.java
@@ -1,3 +1,13 @@
 public class Multiples {
-
+    public static void main(String[] args) {
+        int count = 0;
+        for (int i = 0; i < 1000; i++) {
+            boolean MultipleOf5 = i % 5 == 0;
+            boolean MultipleOf3 = i % 3 == 0;
+            if (MultipleOf5 || MultipleOf3) {
+                count++;
+            }
+        }
+        System.out.println(count);
+    }
 }
